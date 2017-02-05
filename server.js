88a29a4dba786841564c9b2994a4e81bb2afc974
Megan Coyle <meg@mcoyle.com>
2017-02-04 20:10:58 -0500

    Setting up server

---
+++ server.js
@@ -0,0 +1,25 @@
+// call packages needed
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// routes for API
+var router = express.Router();
+
+// test route to make sure it's working
+router.get('/', function(req, res) {
+  res.json({ message: "yays, it's working!" });
+});
+
+// register routes
+app.use('/api', router);
+
+// start the server
+app.listen(port);
+console.log('Listening at ' + port);
